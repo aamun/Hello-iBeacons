258f6e294f57b5e7759165fd59e5728c034eb8e1
Aaron Munguia <aamm89@gmail.com>
2016-06-11 14:30:43 -0500

    - Import Kontakt SDK - Create an instance of beaconManager and requestLocation Always Authorization. - Create a beacon region with a proximity UUID and a string identify - Start Monitoring and Ranging - Implement KTKBeaconManagerDelegate - Show a background color for each beacon, use the nearly beacon.

--- Hello Beacons/ViewController.swift
+++ Hello Beacons/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import KontaktSDK
 
 class ViewController: UIViewController {
-
+    
+    var beaconManager: KTKBeaconManager!
+    var beacon:CLBeacon?
+    let colors = [
+        37515:UIColor.redColor(),
+        51735:UIColor.yellowColor(),
+        7541:UIColor.greenColor()
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initiate Beacon Manager
+        beaconManager = KTKBeaconManager(delegate: self)
+        beaconManager.requestLocationAlwaysAuthorization()
+        
+        // Region
+        let proximityUUID = NSUUID(UUIDString: "f7826da6-4fa2-4e98-8024-bc5b71e0893e")
+        let region = KTKBeaconRegion(proximityUUID: proximityUUID!, identifier: "region")
+        
+        // Start Monitoring and Ranging
+        beaconManager.startMonitoringForRegion(region)
+        beaconManager.startRangingBeaconsInRegion(region)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
+extension ViewController: KTKBeaconManagerDelegate {
+    
+    func beaconManager(manager: KTKBeaconManager, didChangeLocationAuthorizationStatus status: CLAuthorizationStatus) {
+        
+    }
+    
+    func beaconManager(manager: KTKBeaconManager, didEnterRegion region: KTKBeaconRegion) {
+        print("Enter region \(region)")
+    }
+    
+    func beaconManager(manager: KTKBeaconManager, didExitRegion region: KTKBeaconRegion) {
+        print("Exit region \(region)")
+    }
+    
+    func beaconManager(manager: KTKBeaconManager, didRangeBeacons beacons: [CLBeacon], inRegion region: KTKBeaconRegion) {
+        
+        for beacon in beacons {
+            print("UUID: \(beacon.proximityUUID.UUIDString)")
+            print("Proximity: \(beacon.proximity.hashValue)")
+            print("Accuracy: \(beacon.accuracy)")
+            print("RSSi: \(beacon.rssi)")
+            print("Major: \(beacon.major)")
+            print("Minor: \(beacon.minor)")
+        }
+        
+        print("Ranged beacons count: \(beacons.count)")
+        
+        if beacons.count > 0 {
+            view.backgroundColor = colors[(beacons.first?.minor.integerValue)!]
+        }
+    }
+}
